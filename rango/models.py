8d008f362abd78a2eb3b766d8d48ce424aaa4777
Kei kuan To <2498456t@student.gla.ac.uk>
2021-08-03 10:15:35 +0100

    added features for Bingo e.g. support comments

--- rango/models.py
+++ rango/models.py
@@ -61,7 +61,7 @@ class Comment(models.Model):
 
     content = models.CharField(max_length=500)
     rating = models.IntegerField(default=0)
-    log_datetime = models.DateTimeField(auto_now_add=True)
+    log_datetime = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content[0:10]
--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -10,4 +10,30 @@ $(document).ready(function() {
                 $('#like_btn').hide();
             })
         });
+
+   
+    $( "#post_btn" ).click(function( event ) {
+        // Stop form from submitting normally
+        event.preventDefault();
+        var catecategoryIdVar, comments, rating;
+
+        catecategoryIdVar = $(this).attr('data-categoryid');
+        comments = $('#txt_comment').val()
+        rating = $("input[name='RatingRadioOptions']:checked").val();
+
+        $.ajax({
+            type:'GET',
+            url: '/rango/post_comment/',
+            data: {'category_id': catecategoryIdVar, 'comments':comments, 'rating':rating},
+            success: function(data){
+                $('#option_msg').html('Thanks for your opinion.');
+                $('#post_btn').prop('disabled', true);
+                $('#txt_comment').prop('disabled', true);
+                $("input[type=radio]").attr('disabled', true);
+            },
+            contentType: "application/json",
+            dataType: 'json'
+        });
+        
+    });
 });
